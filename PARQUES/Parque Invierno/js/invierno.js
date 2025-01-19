786f5ac418f4e4d7a35f510d55eab62d694e4de4
ANGELMUSEO <angel.gonzalezfdz@gmail.com>
2025-01-19 08:00:16 +0100

    hc

--- PARQUES/Parque Invierno/js/invierno.js
+++ PARQUES/Parque Invierno/js/invierno.js
@@ -9,128 +9,128 @@ $(document).ready(function(){
 
     /*ocultacion de cerrar botones cuando clico en uno*/
          $(".btnin1").click(function(){
-         $(".tarjin1").toggle(1000);
+         $(".tarjin1").toggle(2000);
 
          if($(".tarjin2").is(":visible")){
-            $(".tarjin2").toggle(1000)
+            $(".tarjin2").toggle(2000)
          };
          if($(".tarjin3").is(":visible")){
-            $(".tarjin3").toggle(1000)
+            $(".tarjin3").toggle(2000)
          };
          if($(".tarjin4").is(":visible")){
-             $(".tarjin4").toggle(1000)
+             $(".tarjin4").toggle(2000)
          };
          if($(".tarjin5").is(":visible")){
-            $(".tarjin5").toggle(1000)
+            $(".tarjin5").toggle(2000)
          };
          if($(".tarjin6").is(":visible")){
-            $(".tarjin6").toggle(1000)
+            $(".tarjin6").toggle(2000)
          };
     }) 
         
          $(".btnin2").click(function(){
-         $(".tarjin2").toggle(1000);
+         $(".tarjin2").toggle(2000);
 
          if($(".tarjin1").is(":visible")){
-            $(".tarjin1").toggle(1000)
+            $(".tarjin1").toggle(2000)
          };
          if($(".tarjin3").is(":visible")){
-            $(".tarjin3").toggle(1000)
+            $(".tarjin3").toggle(2000)
          };
          if($(".tarjin4").is(":visible")){
-            $(".tarjin4").toggle(1000)
+            $(".tarjin4").toggle(2000)
          };
          if($(".tarjin5").is(":visible")){
-            $(".tarjin5").toggle(1000)
+            $(".tarjin5").toggle(2000)
          };
          if($(".tarjin6").is(":visible")){
-            $(".tarjin6").toggle(1000)
+            $(".tarjin6").toggle(2000)
          };
 
     }) 
 
          $(".btnin3").click(function(){
-         $(".tarjin3").toggle(1000);
+         $(".tarjin3").toggle(2000);
 
          if($(".tarjin1").is(":visible")){
-            $(".tarjin1").toggle(1000)
+            $(".tarjin1").toggle(2000)
          };
          if($(".tarjin2").is(":visible")){
-            $(".tarjin2").toggle(1000)
+            $(".tarjin2").toggle(2000)
          };
          if($(".tarjin4").is(":visible")){
-            $(".tarjin4").toggle(1000)
+            $(".tarjin4").toggle(2000)
          };
          if($(".tarjin5").is(":visible")){
-            $(".tarjin5").toggle(1000)
+            $(".tarjin5").toggle(2000)
          };
          if($(".tarjin6").is(":visible")){
-            $(".tarjin6").toggle(1000)
+            $(".tarjin6").toggle(2000)
          };
     })
          $(".btnin4").click(function(){
-         $(".tarjin4").toggle(1000);
+         $(".tarjin4").toggle(2000);
 
          if($(".tarjin1").is(":visible")){
-            $(".tarjin1").toggle(1000)
+            $(".tarjin1").toggle(2000)
          };
          if($(".tarjin2").is(":visible")){
-            $(".tarjin2").toggle(1000)
+            $(".tarjin2").toggle(2000)
          };
       
          if($(".tarjin3").is(":visible")){
-            $(".tarjin3").toggle(1000)
+            $(".tarjin3").toggle(2000)
          };
        
          if($(".tarjin5").is(":visible")){
-            $(".tarjin5").toggle(1000)
+            $(".tarjin5").toggle(2000)
          }; 
        
          if($(".tarjin6").is(":visible")){
-            $(".tarjin6").toggle(1000)
+            $(".tarjin6").toggle(2000)
          };
     })   
       
             $(".btnin5").click(function(){
-            $(".tarjin5").toggle(1000);
+            $(".tarjin5").toggle(2000);
 
          if($(".tarjin1").is(":visible")){
-            $(".tarjin1").toggle(1000)
+            $(".tarjin1").toggle(2000)
          };
          if($(".tarjin2").is(":visible")){
-            $(".tarjin2").toggle(1000)
+            $(".tarjin2").toggle(2000)
          };
         
          if($(".tarjin3").is(":visible")){
-            $(".tarjin3").toggle(1000)
+            $(".tarjin3").toggle(2000)
          };
         
          if($(".tarjin4").is(":visible")){
-            $(".tarjin4").toggle(1000)
+            $(".tarjin4").toggle(2000)
          };
          if($(".tarjin6").is(":visible")){
-            $(".tarjin6").toggle(1000)
+            $(".tarjin6").toggle(2000)
          };
     })
        $(".btnin6").click(function(){
-       $(".tarjin6").toggle(1000);
+       $(".tarjin6").toggle(2000);
 
          if($(".tarjin1").is(":visible")){
-            $(".tarjin1").toggle(1000)
+            $(".tarjin1").toggle(2000)
          };
          if($(".tarjin2").is(":visible")){
-            $(".tarjin2").toggle(1000)
+            $(".tarjin2").toggle(2000)
          };
     
          if($(".tarjin3").is(":visible")){
-            $(".tarjin3").toggle(1000)
+            $(".tarjin3").toggle(2000)
          };
     
          if($(".tarjin4").is(":visible")){
-            $(".tarjin4").toggle(1000)
+            $(".tarjin4").toggle(2000)
          };
          if($(".tarjin5").is(":visible")){
-            $(".tarjin5").toggle(1000)
+            $(".tarjin5").toggle(2000)
          };    
    })
 
--- PARQUES/parque oeste/js/oeste.js
+++ PARQUES/parque oeste/js/oeste.js
@@ -24,91 +24,218 @@ $(document).ready(function () {
 
     // al hacer click en los botones muestra los textos y oculta el resto
     $(".botonv-1").click(function () {
-        $(".textv-1").toggle(500);
-        $(".textv-2").hide();
-        $(".textv-3").hide();
-        $(".textv-4").hide();
-        $(".textv-5").hide();
-        $(".textv-6").hide();
-        $(".textv-7").hide();
-        $(".textv-8").hide();
+        $(".textv-1").toggle(1000);
+        if($(".textv-2").is(":visible")){
+            $(".textv-2").toggle(1000)   
+        };
+
+        if($(".textv-3").is(":visible")){
+            $(".textv-3").toggle(1000)  
+        };
+        if($(".textv-4").is(":visible")){
+            $(".textv-4").toggle(1000) 
+        };
+        if($(".textv-5").is(":visible")){
+            $(".textv-5").toggle(1000)
+        };
+        if($(".textv-6").is(":visible")){
+            $(".textv-6").toggle(1000)
+        };
+        if($(".textv-7").is(":visible")){
+            $(".textv-7").toggle(1000) 
+        };
+        if($(".textv-8").is(":visible")){
+            $(".textv-8").toggle(1000)
+        };
 
     });
     $(".botonv-2").click(function () {
-        $(".textv-2").toggle(500);
-        $(".textv-3").hide();
-        $(".textv-4").hide();
-        $(".textv-5").hide();
-        $(".textv-6").hide();
-        $(".textv-7").hide();
-        $(".textv-8").hide();
-        $(".textv-1").hide();
+        $(".textv-2").toggle(1000);
+
+        if($(".textv-3").is(":visible")){
+            $(".textv-3").toggle(1000)  
+        };
+        if($(".textv-4").is(":visible")){
+            $(".textv-4").toggle(1000) 
+        };
+        if($(".textv-5").is(":visible")){
+            $(".textv-5").toggle(1000) 
+        };
+        if($(".textv-6").is(":visible")){
+            $(".textv-6").toggle(1000)  
+        };
+        if($(".textv-7").is(":visible")){
+            $(".textv-7").toggle(1000) 
+        };
+        if($(".textv-8").is(":visible")){
+            $(".textv-8").toggle(1000)
+        };
+        if($(".textv-1").is(":visible")){
+            $(".textv-1").toggle(1000)  
+        };
     });
 
     $(".botonv-3").click(function () {
-        $(".textv-3").toggle(500);
-        $(".textv-4").hide();
-        $(".textv-5").hide();
-        $(".textv-6").hide();
-        $(".textv-7").hide();
-        $(".textv-8").hide();
-        $(".textv-1").hide();
-        $(".textv-2").hide();
+        $(".textv-3").toggle(1000);
+
+        if($(".textv-4").is(":visible")){
+            $(".textv-4").toggle(1000)
+        };
+        
+        
+        if($(".textv-5").is(":visible")){
+            $(".textv-5").toggle(1000) 
+        };
+        if($(".textv-6").is(":visible")){
+            $(".textv-6").toggle(1000) 
+        };
+        if($(".textv-7").is(":visible")){
+            $(".textv-7").toggle(1000) 
+        };
+        if($(".textv-8").is(":visible")){
+            $(".textv-8").toggle(1000) 
+        };
+        if($(".textv-1").is(":visible")){
+            $(".textv-1").toggle(1000) 
+        };
+        if($(".textv-2").is(":visible")){
+            $(".textv-2").toggle(1000)
+        };
     });
 
     $(".botonv-4").click(function () {
-        $(".textv-4").toggle(500);
-        $(".textv-5").hide();
-        $(".textv-6").hide();
-        $(".textv-7").hide();
-        $(".textv-8").hide();
-        $(".textv-1").hide();
-        $(".textv-2").hide();
-        $(".textv-3").hide();
+        $(".textv-4").toggle(1000);
+
+        if($(".textv-5").is(":visible")){
+            $(".textv-5").toggle(1000) 
+        };
+        if($(".textv-6").is(":visible")){
+            $(".textv-6").toggle(1000) 
+        };
+        if($(".textv-7").is(":visible")){
+            $(".textv-7").toggle(1000) 
+        };
+        if($(".textv-8").is(":visible")){
+            $(".textv-8").toggle(1000)
+        };
+        if($(".textv-1").is(":visible")){
+            $(".textv-1").toggle(1000) 
+        };
+        if($(".textv-2").is(":visible")){
+            $(".textv-2").toggle(1000)
+        };
+        
+        if($(".textv-3").is(":visible")){
+            $(".textv-3").toggle(1000) 
+        };
     });
 
     $(".botonv-5").click(function () {
-        $(".textv-5").toggle(500);
-        $(".textv-6").hide();
-        $(".textv-7").hide();
-        $(".textv-8").hide();
-        $(".textv-1").hide();
-        $(".textv-2").hide();
-        $(".textv-3").hide();
-        $(".textv-4").hide();
+        $(".textv-5").toggle(1000);
+
+        if($(".textv-6").is(":visible")){
+            $(".textv-6").toggle(1000) 
+        };
+        if($(".textv-7").is(":visible")){
+            $(".textv-7").toggle(1000) 
+        };
+        if($(".textv-8").is(":visible")){
+            $(".textv-8").toggle(1000) 
+        };
+        if($(".textv-1").is(":visible")){
+            $(".textv-1").toggle(1000) 
+        };
+        if($(".textv-2").is(":visible")){
+            $(".textv-2").toggle(1000)
+        };
+        
+        if($(".textv-3").is(":visible")){
+            $(".textv-3").toggle(1000) 
+        };
+        if($(".textv-4").is(":visible")){
+            $(".textv-4").toggle(1000) 
+        };
     });
 
     $(".botonv-6").click(function () {
-        $(".textv-6").toggle(500);
-        $(".textv-7").hide();
-        $(".textv-8").hide();
-        $(".textv-1").hide();
-        $(".textv-2").hide();
-        $(".textv-3").hide();
-        $(".textv-4").hide();
-        $(".textv-5").hide();
+        $(".textv-6").toggle(1000);
+
+        if($(".textv-7").is(":visible")){
+            $(".textv-7").toggle(1000) 
+        };
+        if($(".textv-8").is(":visible")){
+            $(".textv-8").toggle(1000) 
+        };
+        if($(".textv-1").is(":visible")){
+            $(".textv-1").toggle(1000) 
+        };
+        if($(".textv-2").is(":visible")){
+            $(".textv-2").toggle(1000)
+        };
+        
+        if($(".textv-3").is(":visible")){
+            $(".textv-3").toggle(1000) 
+        };
+        if($(".textv-4").is(":visible")){
+            $(".textv-4").toggle(1000) 
+        };
+        if($(".textv-5").is(":visible")){
+            $(".textv-5").toggle(1000) 
+        };
     });
 
     $(".botonv-7").click(function () {
-        $(".textv-7").toggle(500);
-        $(".textv-8").hide();
-        $(".textv-1").hide();
-        $(".textv-2").hide();
-        $(".textv-3").hide();
-        $(".textv-4").hide();
-        $(".textv-5").hide();
-        $(".textv-6").hide();
+        $(".textv-7").toggle(1000);
+
+        if($(".textv-8").is(":visible")){
+            $(".textv-8").toggle(1000) 
+        };
+        if($(".textv-1").is(":visible")){
+            $(".textv-1").toggle(1000) 
+        };
+        if($(".textv-2").is(":visible")){
+            $(".textv-2").toggle(1000)
+        };
+        
+        if($(".textv-3").is(":visible")){
+            $(".textv-3").toggle(1000) 
+        };
+        if($(".textv-4").is(":visible")){
+            $(".textv-4").toggle(1000) 
+        };
+        if($(".textv-5").is(":visible")){
+            $(".textv-5").toggle(1000) 
+        };
+        if($(".textv-6").is(":visible")){
+            $(".textv-6").toggle(1000) 
+        };
     });
 
     $(".botonv-8").click(function () {
-        $(".textv-8").toggle(500);
-        $(".textv-1").hide();
-        $(".textv-2").hide();
-        $(".textv-3").hide();
-        $(".textv-4").hide();
-        $(".textv-5").hide();
-        $(".textv-6").hide();
-        $(".textv-7").hide();
+        $(".textv-8").toggle(1000);
+
+        if($(".textv-1").is(":visible")){
+            $(".textv-1").toggle(1000) 
+        };
+        if($(".textv-2").is(":visible")){
+            $(".textv-2").toggle(1000)
+        };
+        
+        if($(".textv-3").is(":visible")){
+            $(".textv-3").toggle(1000) 
+        };
+        if($(".textv-4").is(":visible")){
+            $(".textv-4").toggle(1000) 
+        };
+        if($(".textv-5").is(":visible")){
+            $(".textv-5").toggle(1000) 
+        };
+        if($(".textv-6").is(":visible")){
+            $(".textv-6").toggle(1000) 
+        };
+        if($(".textv-7").is(":visible")){
+            $(".textv-7").toggle(1000) 
+        };
     });
 
 
@@ -129,24 +256,39 @@ $(document).ready(function(){
 $(document).ready(function(){
 
     $("#boton1").click(function(){
-        $(".parrafo1").toggle(1500)
-        $(".parrafo2").hide()
-        $(".parrafo3").hide()
+        $(".parrafo1").toggle(1000)
+
+        if($(".parrafo2").is(":visible")){
+            $(".parrafo2").toggle(1000) 
+        }
+        if($(".parrafo3").is(":visible")){
+            $(".parrafo3").toggle(1000) 
+        }
     
 
     })
 
     $("#boton2").click(function(){
-        $(".parrafo2").toggle(1500)
-        $(".parrafo1").hide()
-        $(".parrafo3").hide()
+        $(".parrafo2").toggle(1000)
+
+        if($(".parrafo1").is(":visible")){
+            $(".parrafo1").toggle(1000)
+        }
+        if($(".parrafo3").is(":visible")){
+            $(".parrafo3").toggle(1000) 
+        }
     
     })
     
     $("#boton3").click(function(){
-        $(".parrafo3").toggle(1500)
-        $(".parrafo1").hide()
-        $(".parrafo2").hide()
+        $(".parrafo3").toggle(1000)
+
+        if($(".parrafo1").is(":visible")){
+            $(".parrafo1").toggle(1000)
+        }
+        if($(".parrafo2").is(":visible")){
+            $(".parrafo2").toggle(1000) 
+        }
     
     })
 
